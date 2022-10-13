1848a9111d557c291f38762993937ac86a52cd25
artem <artemkbnv@yandex.ru>
2022-10-13 14:17:58 +0300

    сделал первую задачу

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // дз 8 масивы часть 1.
+        // задача 1
+        int [] array1 =  new int[3];
+        array1[0]=1;
+        array1[1]=2;
+        array1[2]=3;
+        double [] array2 = {1.57, 7.654, 9.986};
+
+    }
+}
